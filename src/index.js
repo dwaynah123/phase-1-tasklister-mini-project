c58bd9952789d30079db581e34110f7ec173e7d0
Dwayna Harris <dwaynaharris@gmail.com>
2023-05-27 18:34:20 -0400

    Practice creating and debugging forms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', (event) => {
+    toDoList(event.target["new-task-description"].value);
+    event.preventDefault();
+    form.reset();
+  })
+}) 
+
+  function toDoList(todo) {
+    let li = document.createElement('li');
+    let button = document.createElement('button');
+    button.addEventListener('click', deleteToDo);
+    button.textContent = 'x';
+    li.textContent = todo + " ";
+    li.appendChild(button);
+    console.log(li);
+    document.querySelector("#tasks").appendChild(li);
+  }
+
+  function deleteToDo(event){
+    event.target.parentNode.remove();
+  }
